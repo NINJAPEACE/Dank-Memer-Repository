133d62b02b8ca05bf67085904889d4f9759e042a
NINJAPEACE <71932430+NINJAPEACE@users.noreply.github.com>
2021-02-01 04:48:39 +0700

    Update index.js

--- index.js
+++ index.js
@@ -8,10 +8,24 @@ const fs = require('fs');
 client.commands = new discord.collection();
 client.aliases = new discord.aliases();
 
-fs.readdir('./commands/')(err, files)=> {
+fs.readdir('./commands/', (err, files) => {
 //to notify when there is an error
 if(err) console.log(err)
-}
+
+//search for files endsWith .js
+let file = files.filter(f => f.endsWith('.js'))
+
+//If you don't make any commands then we will be notified
+if(file.length <= 0) return console.log(`Couldn't find commands!`)
+
+file.forEach((f, i) => {
+let pull = require(`./command/${f}`);
+client.commands.set(pull.config.name, pull);
+pull.config.aliases.forEach(alias => {
+client.aliases.set(alias, pull.config.name);
+		});
+	});
+});
 
 //to notify when the bot is ready to use
 client.on('ready', async() => {
@@ -19,17 +33,16 @@ console.log(`I'm ready`)
 });
 
 client.on('message', async message => {
-
 let prefix = "pls";
 if(message.content.startsWith('PLS')) prefix = 'PLS';
 if(message.content.startsWith('Pls')) prefix = 'Pls';
-//You can add more and customize it
+//You can add more and customize the prefix
 
 if(message.content.startsWith(prefix)) return null;
 
 let args = message.content.slice(prefix.length).trim().split(' ');
 let command = args.shift().toLowerCase();
-let commandfile = client.commands.get(cmd) || client.commands.get(alias(cmd));
+let commandfile = client.commands.get(cmd) || client.commands.get(client.aliases.get(cmd));
 if(commandfile)commandfile.run(client, message, args?;
 
 });
